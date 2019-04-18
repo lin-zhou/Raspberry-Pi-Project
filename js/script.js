3c96401cac71e8b37f3465a37e251b03f490b817
lin-zhou <zhou.lindsay@yahoo.com>
2019-04-17 22:01:08 -0400

    Resume

--- js/script.js
+++ js/script.js
@@ -4,10 +4,15 @@ const playImagePath = 'url(images/Play.png)';
 var isPlaying = false;
 
 // Tokens must be changed every hour
-const apiToken = 'BQDPZZrLZqm5Muz1qxyF7DPvf8DdCIosH3km3W159q92cOT1zd-L2LfeNdbdqiS_wzO6WPWLbJSGVlePltTEip5wk71zKDvUsyRglPCBC5lVit_AEjY0aykJ4R57vu5JRL9-6RjlkZ3b9EC28o2ymAZxFhJbxoKFUE8S-5LnMqWHocqcgDmcfOlUiol7Fkv6I0wt5dFFWw';
-const sdkToken = 'BQCm9kRav_uEVCSg-pqDooGZNiTI3Qm5upa3hLt_0kPWoO_6dZ6YUkv8rQX8res2lkSSS9YGak76YUcN_Fgii9R03V3xr5thSpmfswIyPxcpDcIXBsuM6VKLRDqacGtHe_PmZ_kIV-EZ90qJZkZloPxOb635Wty75GO48n-Ziw';
+const apiToken = '<api token here>';
+const sdkToken = '<sdk token here>';
 
 let thisDeviceID = null;
+
+var info = null;
+
+var startSong = 5;
+var startPosition = 0;
 
 window.onSpotifyWebPlaybackSDKReady = () => {
      const player = new Spotify.Player({
@@ -22,7 +27,10 @@ window.onSpotifyWebPlaybackSDKReady = () => {
      player.addListener('playback_error', ({ message }) => { console.error(message); });
 
      // Playback status updates
-     player.addListener('player_state_changed', state => { console.log(state); });
+     player.addListener('player_state_changed', state => { 
+          console.log(state);
+          startPosition = state.position;
+     });
 
      // Ready
      player.addListener('ready', ({ device_id }) => {
@@ -42,8 +50,6 @@ window.onSpotifyWebPlaybackSDKReady = () => {
 var spotifyApi = new SpotifyWebApi();
 spotifyApi.setAccessToken(apiToken);
 
-var startSong = 5;
-var startPosition = 0;
 function playMusic() {
      var options = {
       device_id: thisDeviceID,
@@ -65,8 +71,6 @@ function playMusic() {
 
 function pauseMusic() {
      spotifyApi.pause();
-     startSong = 5;          // Change this to be whatever song we left off at
-     startPosition = 0;      // It'd be super cool if this could set to the position_ms at time of pausing, but alas
 }
 
 // This function is called when the mainButton element is clicked
